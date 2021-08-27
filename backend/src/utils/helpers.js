649b90c4bfe7dabcb0517cf425b029a00ac9906e
Dennis Mubamba <dennism501@outlook.com>
2021-08-27 09:38:44 +0200

    :construction: Adding handling to the http requests

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -3,9 +3,13 @@ const fetch = require("node-fetch");
 module.exports.useFetch = async (url) => {
   try {
     const api_key = process.env.API_KEY;
-    const result = await fetch(`${url}?api_key=${api_key}`);
-    const res = await result.json();
-    return res;
+    const result = await fetch(`${url}?api_key=${"api_key"}`);
+    console.log(result.ok);
+
+    if (!result.ok) {
+      return await result.json();
+    }
+    return await result.json();
   } catch (err) {
     console.error(err);
   }
